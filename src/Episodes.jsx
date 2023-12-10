1153c017d1bd80af44f8ee0c5ae1d909be9e4512
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2023-12-10 02:30:56 +0100

    Adding few necessary functions and testing them

--- src/Episodes.jsx
+++ src/Episodes.jsx
@@ -8,17 +8,38 @@ import { GET_EPISODES } from "./gql/Query"
 function Episodes() {
   // const [episodes, setEpisodes] = useState(0);
   const { data, loading, error } = useQuery(GET_EPISODES);
-  console.log(data);
+  
+  function episodeNumber(epNumber){
+    if(epNumber<10){
+      const numberString = epNumber.toString().padStart(2,'0');
+      return numberString}
+      else {return epNumber}
+    }
+
+  function episodeDetailsSite(episodeId){
+
+  }
 
   if (loading) return "Loading...";
   if (error) return <pre>{error.message}</pre>
 
+  
+
   return (
     <>
       <section Id='mainsection'> 
-      {data.episodes.results.map((episode) => (
-        <p key={episode.id}>{episode.name}</p>
-        ))}                  
+        <div className='verticalline'></div>
+          <div className='episodeidbracket'>
+          {data.episodes.results.map((episode) => (    
+          <div key={episode.id}>             
+           <p className="episodeid">S04E{episodeNumber(episode.id)}</p>
+              <div className='episodedescription'>
+                <a href="#" className="enameblue">{episode.name}</a>
+                <p className="emisiondate">{episode.air_date}</p>
+              </div>  
+          </div> ))} 
+          </div>                 
+                        
       </section>
      
     </>
@@ -36,3 +57,11 @@ export default Episodes
             <p className="emisiondate">{episode.Air_Date}</p>
           </div>          
         </div> */}
+
+      //   <section Id='mainsection'> 
+      // {data.episodes.results.map((episode) => (
+      //   <p key={episode.id}>{episode.name}</p>
+      //   ))}                  
+      // </section>
+
+      

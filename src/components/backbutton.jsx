af3236da12a37d2d7e817dd45d77092da76f6dc1
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-01-20 01:32:30 +0100

    Changing backbutton

--- src/components/backbutton.jsx
+++ src/components/backbutton.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "../Style.css";
-import BackImage from "../image/Refund_back.png";
-import { useParams, useNavigate } from "react-router-dom";
+import BackImage from "../Images/Refund_back.png";
+import {
+  useParams,
+  useNavigate,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
 function BackButton() {
   const navigate = useNavigate();
+  const episodesNumber = "%%";
+  function previousPageName() {
+    if (window.location.pathname == `/episode/${episodesNumber}`) {
+      return "Episodes";
+    } else {
+      return "Characters";
+    }
+  }
 
   if (window.location.pathname == "/") {
     return <div></div>;
@@ -13,11 +27,40 @@ function BackButton() {
       <React.Fragment>
         <button onClick={() => navigate(-1)} className="backbutton">
           <img src={BackImage} alt="" sizes="" srcset="" />
-          Back
+          {(previousPageName = previousPageName())}
         </button>
       </React.Fragment>
     );
   }
 }
+
+//   switch (window.location.pathname) {
+//     case "/":
+//       return <div></div>;
+//       break;
+
+//     case `/episode/:id`:
+//       return (
+//         <React.Fragment>
+//           <button onClick={() => navigate(-1)} className="backbutton">
+//             <img src={BackImage} alt="" sizes="" srcset="" />
+//             Episodes
+//           </button>
+//         </React.Fragment>
+//       );
+//       break;
+
+//     case `/character/:id`:
+//       return (
+//         <React.Fragment>
+//           <button onClick={() => navigate(-1)} className="backbutton">
+//             <img src={BackImage} alt="" sizes="" srcset="" />
+//             Characters
+//           </button>
+//         </React.Fragment>
+//       );
+//       break;
+//   }
+// }
 
 export default BackButton;
